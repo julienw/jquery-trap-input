9ff2773632242b64808dfb3d07f6a26f533df81a
Julien Wajsberg <felash@gmail.com>
2011-07-21 08:42:06 +0200

    can also use sundown instead of upskirt, and don't replace the examples part, the link is now directly in the README

--- buildsite.sh
+++ buildsite.sh
@@ -5,8 +5,14 @@ BUILD_DIR=website-dist
 
 if which upskirt > /dev/null ; then
     echo "we found upskirt, that's good, continuing."
+    parser="upskirt";
+elif which sundown > /dev/null ; then
+	echo "we found sundown, that's good, continuing."
+	parser="sundown";
 else
-    echo "upskirt was not found in the PATH, exiting."
+    echo "sundown was not found in the PATH, exiting."
+    echo "Clone https://github.com/tanoku/sundown.git to download it and"
+    echo "then make it."
     exit
 fi
 
@@ -14,7 +20,7 @@ rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 cp -r $DIR/js $DIR/css $BUILD_DIR/
 
-upskirt README.md | sed -e 's/<pre><code class="/<pre class="sh_/' -e 's/<\/code><\/pre>/<\/pre>/' -e 's/<code>examples<\/code>/<a href="examples">examples<\/a>/' > ${BUILD_DIR}/index.pre.html
+${parser} README.md | sed -e 's/<pre><code class="/<pre class="sh_/' -e 's/<\/code><\/pre>/<\/pre>/' > ${BUILD_DIR}/index.pre.html
 
 cat $DIR/head.html ${BUILD_DIR}/index.pre.html $DIR/foot.html > ${BUILD_DIR}/index.html
 
